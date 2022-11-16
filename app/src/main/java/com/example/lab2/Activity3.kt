d25f512002fc29789352964700af1bf03e8bd513
adarshchimnani <67822194+adarshchimnani@users.noreply.github.com>
2022-11-16 13:06:27 +0500

    Basic Cafe Coffee App - Initilized Buttons, Texts, Checkbox

---
+++ app/src/main/java/com/example/lab2/Activity3.kt
@@ -0,0 +1,38 @@
+package com.example.lab2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.TextView
+
+class Activity3 : AppCompatActivity() {
+
+    lateinit var creamCheck: CheckBox;
+    lateinit var chocoCheck: CheckBox;
+    lateinit var subBtn: Button;
+    lateinit var addBtn: Button;
+    lateinit var txtQt: TextView;
+    lateinit var orderBtn: Button;
+    lateinit var txtSummary: TextView;
+    var quantity = 0;
+    var cream = "";
+    var choco = "";
+    var price = 0;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_3)
+
+        creamCheck = findViewById(R.id.checkCream);
+        chocoCheck = findViewById(R.id.checkChoco);
+        subBtn = findViewById(R.id.subBtn);
+        addBtn = findViewById(R.id.addBtn);
+        txtQt = findViewById(R.id.txtQt);
+        orderBtn = findViewById(R.id.orderBtn);
+        txtSummary = findViewById(R.id.txtSummary);
+    }
+
+
+}
